e1ce8a782c923521baa6e56e9a8ec23d5256c74c
LarsenKyle <krlars87@gmail.com>
2021-01-27 15:16:58 -0500

    Inital Commit

---
+++ components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+      <img src="~/static/logo.svg" alt="" class="logo">
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+img{
+    width: 10vw;
+    max-width: 75px;
+    display: block;
+}
+nav{
+    position: fixed;
+    background-color: inherit;
+    width:100vw;
+    top: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <Nav/>
+    <img src="~/static/mid-img.svg" alt="">
+  </div>
+</template>
+<script>
+export default {}
+</script>
+
+<style scoped>
+img{
+  margin-top: 10vh;
+  width: 90vw;
+  max-width: 500px;
+}
+.container {
+  position: relative;
+  margin: 0 auto;
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
